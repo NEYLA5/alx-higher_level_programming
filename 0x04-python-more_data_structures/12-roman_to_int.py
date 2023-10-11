34a54b6b83645af5eb9f2732841206031fdee080
NEYLA5 <fortpointgraphics@gmail.com>
2023-10-11 02:39:48 -0700

    files

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -38,20 +38,3 @@ def roman_to_int(roman_string):
     num += to_subtract(list_num)
 
     return (num)
-
-=================================
-100-weight_average.py
-
-#!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-
-    num = 0
-    den = 0
-
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
-
-    return (num / den)
